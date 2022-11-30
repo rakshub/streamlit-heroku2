35d86e144b33a5b8e1a0dc47bbb88e40850078fe
rakshub <108409142+rakshub@users.noreply.github.com>
2022-11-30 22:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.write("""
 #TDS Week 8 Assignment
 
-## Sum of two numbers and depolying using Heroku""")
+## Subtract of two numbers and depolying using Heroku""")
 
 a = st.number_input("Enter the first number")
 b = st.number_input("Enter the second number")
 
 s = a-b 
-st.write("Sum of the two numbers are:",s)
+st.write("Subtraction of the two numbers are:",s)
 st.write("Credits: 21f1005104")
